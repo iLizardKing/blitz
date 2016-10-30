794cef630f69b85a594ce07fc4591a45b191aab1
Kuzmin Yevgeniy <eakuzmin87@mail.ru>
2016-10-30 20:20:30 +0300

    ver. 1.2.2
    edit the interface and some chenges in a subjects download

--- human-sort-v1-2.py
+++ human-sort-v1-2.py
@@ -18,45 +18,57 @@ class Application:
     def create_widgets(self):
         self.root = Tk()
         self.root.title('one-FROM-two')
-        self.root.geometry('375x345+50+50')
+        self.root.geometry('375x367+50+50')
         self.root.resizable(False, False)
-        self.fnt1 = ('Arial','18')
-        self.fnt2 = ('Arial','10')
+        self.root.configure(background='white')
+        self.fnt1 = ('Consolas','18')
+        self.fnt2 = ('Consolas','10')
+        self.fnt3 = ('Arial','10')
         self.create_frame1()
         self.create_frame2()
+        self.mess_lab = Label(self.root, font=self.fnt3, bg='black', fg='#00FF00')
+        self.mess_lab['text'] = "Add subjects to the text field with keybord or from the file!"
+        self.mess_lab.place(x=5, y=5, width=365, height=35)
 
     def create_frame1(self):
-        self.frame1 = Frame(self.root, bg='grey', bd=5, height=65, width=365, highlightbackground='red')
-        Label(self.frame1, text="or", font=self.fnt1, bg='grey').place(x=163, y=13)
-        self.option_but1 = Button(self.frame1, font=self.fnt1, width=10, command=self.choose1)
-        self.option_but1['text'] = 'Option 1'
+        self.frame1 = Frame(self.root, bg='#d7edff', bd=5, height=66, width=365)
+        self.frame1.place(x=5, y=44)
+        Label(self.frame1, text="or", font=self.fnt1, bg='#d7edff').place(x=160, y=13)
+        # buttons
+        self.option_but1 = Button(self.frame1, text='one', font=self.fnt1, bd=5, command=self.choose1)
         self.option_but1['state'] = DISABLED
-        self.option_but2 = Button(self.frame1, font=self.fnt1, width=10, command=self.choose2)
-        self.option_but2['text'] = 'Option 2'
+        self.option_but1.place(x=2, y=1, width=150)
+        self.option_but2 = Button(self.frame1, text='two', font=self.fnt1, bd=5, command=self.choose2)
         self.option_but2['state'] = DISABLED
-        self.frame1.place(x=5, y=5)
-        self.option_but1.place(x=5, y=5)
-        self.option_but2.place(x=200, y=5)
+        self.option_but2.place(x=198, y=1, width=155)
 
     def create_frame2(self):
-        self.frame2 = Frame(self.root, bg='#ffffe0', bd=5, height=265, width=365)
+        self.frame2 = Frame(self.root, bg='#ffffe0', bd=5, height=247, width=365)
+        self.frame2.place(x=5, y=115)
+        # labels
         self.quest_num_lab = Label(self.frame2, text="Number of the question: 0", font=self.fnt2, bg='#ffffe0')
-        self.count_elem_lab = Label(self.frame2, text="Count of elements: 0", font=self.fnt2, bg='#ffffe0')
-        self.count_posb_quest_lab = Label(self.frame2, text="Count of possible questions: 0", font=self.fnt2, bg='#ffffe0')
-        self.time_lab = Label(self.frame2, text="Time: 00:00:00.0", font=self.fnt2, bg='#ffffe0')
-        self.options_txt = Text(self.frame2, height=15, width=15, font=self.fnt2)
-        self.add_from_file_but = Button(self.frame2, text="Add from file", font=self.fnt2, command=self.add_from_file)
-        self.add_from_text_but = Button(self.frame2, text="Add", font=self.fnt2, command=self.add_from_text)
-        self.start_but = Button(self.frame2, text='Start', font=self.fnt1, command=self.start)
-        self.add_from_file_but.place(x=225, y=222, width=125)
-        self.add_from_text_but.place(x=125, y=222, width=90)
-        self.options_txt.place(x=5, y=5)
-        self.time_lab.place(x=125, y=65)
-        self.count_posb_quest_lab.place(x=125, y=45)
-        self.count_elem_lab.place(x=125, y=25)
         self.quest_num_lab.place(x=125, y=5)
-        self.frame2.place(x=5, y=75)
-        self.start_but.place(x=125, y=160, width=225)
+        self.count_elem_lab = Label(self.frame2, text="Count of elements: 0", font=self.fnt2, bg='#ffffe0')
+        self.count_elem_lab.place(x=125, y=25)
+        self.count_posb_quest_lab = Label(self.frame2, text="Count of possible questions: 0", font=self.fnt2, bg='#ffffe0')
+        self.count_posb_quest_lab.place(x=125, y=45)
+        self.time_lab = Label(self.frame2, text="Time: 00:00:00.0", font=self.fnt2, bg='#ffffe0')
+        self.time_lab.place(x=125, y=65)
+        # text field
+        self.options_txt = Text(self.frame2, height=15, width=15, font=self.fnt2, bd=0)
+        self.options_txt.place(x=5, y=5)
+        self.border_for_txt = Frame(self.frame2, width=115, height=235, borderwidth=30)
+        self.border_for_txt['bg'] = 'red'
+        self.border_for_txt.lower()
+        self.border_for_txt.place(x=1, y=1)
+        # buttons
+        self.start_but = Button(self.frame2, text='START', font=self.fnt1, command=self.start)
+        self.start_but['state'] = DISABLED
+        self.start_but.place(x=125, y=154, width=229)
+        self.add_from_text_but = Button(self.frame2, text="Add", font=self.fnt2, command=self.add_from_text)
+        self.add_from_text_but.place(x=125, y=210, width=96)
+        self.add_from_file_but = Button(self.frame2, text="Add from file", font=self.fnt2, command=self.add_from_file)   
+        self.add_from_file_but.place(x=229, y=210, width=125)
 
     def add_from_text(self):
         self.option_but1['state'] = DISABLED
@@ -66,14 +78,21 @@ class Application:
         self.start_but['state'] = NORMAL
         self.timer = False
         self.list_objects = []
-        for subj in self.options_txt.get('1.0', END).split('\n'):
-            if subj:
-                self.list_objects.append( [subj.strip().lower().title(), 0] )
-        random.shuffle(self.list_objects)
-        if self.list_objects:
-            print('list download from text field is done:', self.list_objects, sep='\n')
+        list_objects_view = []
+        lines_text = self.options_txt.get('1.0', END).strip(' \n').split('\n')
+        if lines_text:
+            for subj in lines_text:
+                tmp = subj.strip().lower().title()
+                self.list_objects.append( [tmp, 0] )
+                list_objects_view.append( tmp )
+            random.shuffle( self.list_objects )
+            list_objects_view.sort()
+            list_objects_view = '\n'.join(list_objects_view)
+            self.options_txt.delete('1.0', END) 
+            self.options_txt.insert('1.0', list_objects_view)
+            print('List download from text field is done:', self.list_objects, sep='\n')
         else:
-            print('list is empty:', self.list_objects)
+            print('Text field is empty...')
 
     def add_from_file(self):
         self.option_but1['state'] = DISABLED
@@ -86,22 +105,28 @@ class Application:
         fn = filedialog.askopenfilename(filetypes = [('*.txt files', '*.txt')])
         if fn:
             fin = open(fn, 'r')
-            lines_file = fin.readlines()
+            lines_file = fin.read().strip(' \n').split('\n')
+            print(1,lines_file)
             fin.close()
-            for el in lines_file:
-                self.list_objects.append( [el[:-1], 0] )
-            random.shuffle(self.list_objects)
-            lines_file.sort()
-            list_objects_view = ''.join(lines_file)
-            self.options_txt.delete('1.0', END) 
-            self.options_txt.insert('1.0', list_objects_view)
-            self.count_elem_lab['text'] = self.count_elem_lab['text'][:19] + str(len(self.list_objects))
-            self.count_posb_quest_lab['text'] = self.count_posb_quest_lab['text'][:29] + \
-                                       str(len(self.list_objects)*(len(self.list_objects)-1)//2)
-        if self.list_objects:
-            print('List download from file is done:', self.list_objects, sep='\n')
-        else:
-            print('List is empty:', self.list_objects)
+            if lines_file == ['']:
+               print('File is empty...')
+            elif len(lines_file) < 2:
+                print("Count of the elements less then 2...")
+            else:
+                list_objects_view = []
+                for subj in lines_file:
+                    tmp = subj.strip().lower().title()
+                    self.list_objects.append( [tmp, 0] )
+                    list_objects_view.append( tmp )
+                random.shuffle( self.list_objects )
+                list_objects_view.sort()
+                list_objects_view = '\n'.join(list_objects_view)
+                self.options_txt.delete('1.0', END) 
+                self.options_txt.insert('1.0', list_objects_view)
+                self.count_elem_lab['text'] = self.count_elem_lab['text'][:19] + str(len(self.list_objects))
+                self.count_posb_quest_lab['text'] = self.count_posb_quest_lab['text'][:29] + \
+                                           str(len(self.list_objects)*(len(self.list_objects)-1)//2)
+                print('List download from file is done:', self.list_objects, sep='\n')
 
     def update_time(self):
         if self.timer:
@@ -205,5 +230,7 @@ class Application:
         self.human_sort(self.temp_question[1], self.temp_question[0])
         self.quest_num_lab['text'] = self.quest_num_lab['text'][:24] + str(self.question_number)
 
-app = Application()
-  
+
+if __name__ == '__main__':
+    app = Application()
+    app.root.mainloop()
